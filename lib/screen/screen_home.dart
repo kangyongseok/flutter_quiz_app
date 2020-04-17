7b2cf5b32e18c7ef8019df8616ed738b53b58bf0
강용석 <kangyongsuek@gmail.com>
2020-04-17 23:49:06 +0900

    create: home widget

--- lib/screen/screen_home.dart
+++ lib/screen/screen_home.dart
@@ -17,7 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: Text('My Quiz App'),
           backgroundColor: Colors.deepPurple,
-          leading: Container(),
+          leading: Container(), // 페이지 이동시에 자동으로 생겨나는 뒤로가기 버튼 등을 지우는 효과
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -25,10 +25,79 @@ class _HomeScreenState extends State<HomeScreen> {
           children: <Widget>[
             Center(
               child: Image.asset('images/smoking.png', width: width * 0.8),
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.024),
+            ),
+            Text(
+              '플러터 퀴즈 앱',
+              style: TextStyle(
+                fontSize: width * 0.065,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '퀴즈를 풀기전 안내사항입니다.\n꼼꼼히 읽고 퀴즈 풀기를 눌러주세요.',
+              textAlign: TextAlign.center,
+            ),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            _buildStep(width, '1. 랜덤으로 나오는 퀴즈 3개를 풀어보세요.'),
+            Padding(
+              padding: EdgeInsets.all(width * 0.048),
+            ),
+            Container(
+              padding: EdgeInsets.only(bottom: width * 0.036),
+              child: Center(
+                child: ButtonTheme(
+                  minWidth: width * 0.8,
+                  height: height * 0.05,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  child: RaisedButton(
+                    child: Text(
+                      '지금 퀴즈 풀기',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    color: Colors.deepPurple,
+                    onPressed: () {},
+                  )
+                )
+              )
             )
           ],
         )
       ),
+    );
+  }
+
+  Widget _buildStep(double width, String title) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.048,
+        width * 0.024,
+        width * 0.048,
+        width * 0.024,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Icon(
+            Icons.check_box,
+            size: width * 0.04,
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: width * 0.024),
+          ),
+          Text(
+            title,
+          )
+        ],
+      )
     );
   }
 }
